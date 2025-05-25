79011e7427f80671d8cbbc343eac4b2da2a61a41
Shane Stone <medicstone4@gmail.com>
2025-05-25 18:29:42 +0000

    commit

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCFr2zyXrO9LHF3dgbC-egYXQ3B5xm8fX0",
+  authDomain: "icontrolbell.firebaseapp.com",
+  projectId: "icontrolbell",
+  storageBucket: "icontrolbell.firebasestorage.app",
+  messagingSenderId: "850499660933",
+  appId: "1:850499660933:web:c0fc42bafd67828af10dec"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
