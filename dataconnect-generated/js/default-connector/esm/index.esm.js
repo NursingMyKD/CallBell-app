faeb2f82777a6680417a96051d2dbd40d94ed182
Shane Stone <medicstone4@gmail.com>
2025-06-15 01:40:48 +0000

    commit

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'iControlBell',
+  location: 'us-east1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'iControlBell',
+  location: 'us-east1'
+};
+exports.connectorConfig = connectorConfig;
