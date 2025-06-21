e773730b1a50b4ea1f0f7ce404399a103a3f03e3
Shane Stone <medicstone4@gmail.com>
2025-06-21 22:17:46 +0000

    Make the following changes:
    On the page src/app/page.tsx,
    
    Find this el

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -9,20 +9,16 @@ const Logo = ({ className }: { className?: string }) => (
       xmlns="http://www.w3.org/2000/svg"
       fill="currentColor"
     >
-      <g>
-        {/* Bell Cloche */}
-        <path d="M14 44 C 14 33 22 24 32 24 S 50 33 50 44" />
-        <rect x="12" y="44" width="40" height="5" rx="2" />
-        <path d="M32 20 a3 3 0 0 1 0 -6 a3 3 0 0 1 0 6" />
+        {/* Bell Shape */}
+        <path d="M14 52 C 14 38, 50 38, 50 52" />
+        <rect x="12" y="52" width="40" height="5" rx="2" />
         
-        {/* Phone Receiver */}
-        {/* Cord */}
-        <path d="M43 51 A 16 16 0 0 1 20 42" fill="none" stroke="currentColor" strokeWidth="4" strokeLinecap="round" />
-        {/* Mouthpiece */}
-        <path transform="translate(5, 5)" d="M42 42 C 52 42 52 56 42 56 L 38 56 L 38 42 Z" />
-        {/* Earpiece */}
-        <path transform="translate(-4, -4)" d="M22 28 C 12 28 12 42 22 42 L 26 42 L 26 28 Z" />
-      </g>
+        {/* Phone Receiver on top */}
+        <g transform="translate(0, -5)">
+            <path d="M22 34 C 22 24, 42 24, 42 34" stroke="currentColor" strokeWidth="8" fill="none" />
+            <circle cx="22" cy="34" r="8" />
+            <circle cx="42" cy="34" r="8" />
+        </g>
     </svg>
   </div>
 );
