a570be54a4499b8ee9e828e602071d26ce11b702
Shane Stone <medicstone4@gmail.com>
2025-06-22 16:20:43 +0000

    starting build "60dae97e-d898-4dd9-a335-3d07a1f4dbc6" FETCHSOURCE hint:

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone', // Added for serverful deployment
-  experimental: {
-    allowedDevOrigins: [
-      'https://6000-idx-studio-1746049973989.cluster-ux5mmlia3zhhask7riihruxydo.cloudworkstations.dev',
-    ],
-  },
   images: {
     remotePatterns: [
       // Ensure this array is empty if no patterns are active,
