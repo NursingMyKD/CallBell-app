f07707f6a0024f8b6d88c2c9d6eeb8edaed57b72
Shane Stone <Medicstone4@gmail.com>
2025-07-07 13:55:33 -0400

    file cleanup

--- iControlbell/iControlbell/scripts/swiftlint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# SwiftLint script for iControlbell project
-if which swiftlint >/dev/null; then
-  swiftlint
-else
-  echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
-fi
-exit 0
